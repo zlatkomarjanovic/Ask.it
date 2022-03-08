24ef2df77912ac8f9c1a7b78b717aa0afb0b7e94
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2022-03-08 15:44:28 +0100

    login and register backend
    login and register backend

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 const pool = require('../db');
-
+const bcrypt = require('bcrypt');
+const jwtGenerator = require('../utils/jwtGenerator');
 //registering
 
 router.post('/register', async (req, res) => {
-	const { email, name, password } = req.body;
+	const { ime, prezime, email, password } = req.body;
 	try {
-		const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [
+		const user = await pool.query('SELECT * FROM users WHERE email = $1', [
 			email,
 		]);
 
-		if (user.rows.length > 0) {
+		if (user.rows.length !== 0) {
 			return res.status(401).json('User already exist!');
 		}
 
@@ -18,16 +19,37 @@ router.post('/register', async (req, res) => {
 		const bcryptPassword = await bcrypt.hash(password, salt);
 
 		let newUser = await pool.query(
-			'INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *',
-			[name, email, bcryptPassword]
+			'INSERT INTO users (ime, prezime, email, password) VALUES ($1, $2, $3, $4) RETURNING *',
+			[ime, prezime, email, bcryptPassword]
 		);
 
-		const jwtToken = jwtGenerator(newUser.rows[0].user_id);
+		const token = jwtGenerator(newUser.rows[0].user_id);
 
-		return res.json({ jwtToken });
+		return res.json({ token });
 	} catch (error) {
 		console.error(error.message);
 		res.status(500).send('Server error');
+	}
+});
+
+router.post('/login', async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [
+			email,
+		]);
+
+		if (user.rows.length === 0) {
+			return res.status(401).json('Password or Email is incorrect!');
+		}
+
+		const validPassword = bcrypt.compare(password, user.rows[0].user_password);
+
+		console.log(validPassword);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server Error');
 	}
 });
 
