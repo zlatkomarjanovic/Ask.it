01755dd6c18b97ad85ad8cf28b0db65f04cebc44
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2022-03-08 17:57:07 +0100

    creating register
    creating register - don't forget to seperate logic in a different component later

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,33 +1,54 @@
 import React, { useState } from 'react';
 
 const Register = () => {
+	const [inputs, setInputs] = useState({
+		ime: '',
+		prezime: '',
+		email: '',
+		password: '',
+	});
+
+	const { ime, prezime, email, password } = inputs;
+
+	const onChange = (e) => {
+		setInputs({ ...inputs, [e.target.name]: [e.target.value] });
+	};
+
 	return (
 		<div className='container'>
-			<h1>Register</h1>
+			<h1 className='text-center my-5'>Register</h1>
 			<form>
 				<input
 					type='text'
-					name='name'
+					name='ime'
 					placeholder='Ime'
 					className='form-control my-3'
+					value={ime}
+					onChange={(e) => onChange(e)}
 				/>
 				<input
 					type='text'
-					name='surname'
+					name='prezime'
 					placeholder='Prezime'
 					className='form-control my-3'
+					value={prezime}
+					onChange={(e) => onChange(e)}
 				/>
 				<input
 					type='email'
 					name='email'
 					placeholder='E-mail'
 					className='form-control my-3'
+					value={email}
+					onChange={(e) => onChange(e)}
 				/>
 				<input
 					type='password'
 					name='password'
 					placeholder='Password'
 					className='form-control my-3'
+					value={password}
+					onChange={(e) => onChange(e)}
 				/>
 				<button className='btn btn-info btn-block'>Submit</button>
 			</form>
