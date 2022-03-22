41d8810579b23697eea9f4492150307bf8dae99a
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2022-03-23 00:21:36 +0100

    <yy
    ycyc

--- client/src/pages/MyQuestions/MyQuestions.jsx
+++ client/src/pages/MyQuestions/MyQuestions.jsx
@@ -1,28 +1,55 @@
 import React from 'react';
 import MyQuestionsLogic from './MyQuestionsLogic';
 import { SingleQuestion } from '../../components';
+import { NavLink } from 'react-router-dom';
 
 const MyQuestions = () => {
 	const { newQuestions, setLoadMore, loadMore } = MyQuestionsLogic();
 
 	return (
 		<div className='container'>
-			<h3 className='m-5'>Some of the questions you recently asked!</h3>
-			{newQuestions.map((question) => {
-				return (
-					<SingleQuestion
-						key={question.question_id}
-						question={question}
-						color='bg-primary'
+			{newQuestions.length > 0 ? (
+				<h3 className='m-5'>Some of the questions you recently asked!</h3>
+			) : (
+				<></>
+			)}
+			{newQuestions.length > 0 ? (
+				<>
+					{newQuestions.map((question) => {
+						return (
+							<SingleQuestion
+								key={question.question_id}
+								question={question}
+								color='bg-primary'
+							/>
+						);
+					})}
+				</>
+			) : (
+				<div className='container mb-5 text-center'>
+					<img
+						alt='Nothing here bro, go out and live a little'
+						src='https://thelocalswpg.com/images/empty_item.svg'
 					/>
-				);
-			})}
-			<button
-				onClick={() => setLoadMore(loadMore + 2)}
-				className='btn btn-block btn-primary p-3'
-			>
-				Load more
-			</button>
+					<h1>Well it looks empty in here! Go ask something!</h1>
+				</div>
+			)}
+
+			{newQuestions.length > 5 ? (
+				<button
+					onClick={() => setLoadMore(loadMore + 2)}
+					className='btn btn-block btn-primary p-3'
+				>
+					Load more
+				</button>
+			) : (
+				<>
+					<h4>Go ask some more!</h4>
+					<NavLink className='btn btn-block btn-primary p-2' to='/questions'>
+						Questions
+					</NavLink>
+				</>
+			)}
 		</div>
 	);
 };
